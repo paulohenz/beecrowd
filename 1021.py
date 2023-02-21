1139cd658f9d2b9d861459d58dfd7637981ae143
Paulo Henz <48599068+paulohenz@users.noreply.github.com>
2023-02-20 22:24:42 -0300

    Alguns desafios

---
+++ 1021.py
@@ -0,0 +1,52 @@
+n = 91.01
+
+n100 = n // 100
+n = n - n100*100
+
+n50 = n // 50
+n = n - n50*50
+
+n20 = n // 20
+n = n - n20*20
+
+n10 = n // 10
+n = n - n10*10
+
+n5 = n // 5
+n = n - n5*5
+
+n2 = n // 2
+n = n - n2*2
+
+m100 = n // 1
+n = n - m100*1
+
+m50 = n // 0.5
+n = n - m50*0.5
+
+m25 = n // 0.25
+n = n - m25*0.25
+
+m10 = n // 0.10
+n = n - m10*0.10
+
+m5 = n // 0.05
+n = n - m5*0.05
+
+m1 = n // 0.01
+n = n - m1*0.01
+
+print("NOTAS:")
+print('{} nota(s) de R$ 100.00'.format(int(n100)))
+print('{} nota(s) de R$ 50.00'.format(int(n50)))
+print('{} nota(s) de R$ 20.00'.format(int(n20)))
+print('{} nota(s) de R$ 10.00'.format(int(n10)))
+print('{} nota(s) de R$ 5.00'.format(int(n5)))
+print('{} nota(s) de R$ 2.00'.format(int(n2)))
+print("MOEDAS:")
+print('{} moeda(s) de R$ 1.00'.format(int(m100)))
+print('{} moeda(s) de R$ 0.50'.format(int(m50)))
+print('{} moeda(s) de R$ 0.25'.format(int(m25)))
+print('{} moeda(s) de R$ 0.10'.format(int(m10)))
+print('{} moeda(s) de R$ 0.05'.format(int(m5)))
+print('{} moeda(s) de R$ 0.01'.format(int(m1)))
---
+++ 1036.py
@@ -0,0 +1,13 @@
+import math
+
+a, b, c = map(float, input().split())
+
+delta = b**2 - 4*a*c
+
+if a == 0 or delta < 0:
+    print("Impossivel calcular")
+else:
+    x1 = (-b + math.sqrt(delta)) / (2*a)
+    x2 = (-b - math.sqrt(delta)) / (2*a)
+    print(f"R1 = {x1:.5f}")
+    print(f"R2 = {x2:.5f}")
